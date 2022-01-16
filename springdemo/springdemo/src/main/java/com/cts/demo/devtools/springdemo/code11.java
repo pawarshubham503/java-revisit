7b7e82bcb49b7fffe0a59cedd5840de9bcce9eb4
Shubham <shubhampawar503@gmail.com>
2022-01-16 11:55:42 +0530

    Code 11 to add sum of integers in a string

--- springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code11.java
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code11.java
@@ -1,7 +1,24 @@
 package com.cts.demo.devtools.springdemo;
+//19
 
 public class code11 {
-public static void main(String[] args) {
-	int i;
-}
+	public static void sumOfDigits(String s1) {
+		int sum = 0;
+		for (int i = 0; i < s1.length(); i++) {
+			char a = s1.charAt(i);
+			if (Character.isDigit(a)) {
+				int b = Integer.parseInt(String.valueOf(a));
+				sum = sum + b;
+			}
+		}
+		if (sum==0) {
+			System.out.println(-1);
+		}
+		else
+			System.out.println(sum);
+	}
+	public static void main(String[] args) {
+		String s1="good22bad";
+		sumOfDigits(s1);
+	}
 }
