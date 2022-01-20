0fb4581d506f48818b0b1f8f047282df0ecf4b88
Shubham <shubhampawar503@gmail.com>
2022-01-20 14:28:53 +0530

    check unique number code

--- springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code28.java
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code28.java
@@ -1,5 +1,40 @@
 package com.cts.demo.devtools.springdemo;
 
-public class code28 {
+import java.util.Scanner;
 
+/*Unique Number
+Write a program that accepts an Integer as input and finds whether the number is
+ Unique or not. Print Unique if the number is Unique , else print Not Unique .
+*/
+public class code28 {
+	public static void main(String[]args)
+	{
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int []a=new int[100];
+	int i=0,count=0;
+	while(n!=0)
+	{
+	int num=n%10;
+	a[i]=num;
+	i++;
+	n=n/10;
+	}
+	for(int j=0;j<i-1;j++)
+	{
+	for(int k=j+1;k<=i-1;k++)
+	{
+	if(a[j]==a[k]){
+	count++;
+	}
+	}}
+	if(count>0)
+	{
+	System.out.println("Invalid");
+	}
+	else
+	{
+	System.out.println("valid");
+	}
+	}
 }
