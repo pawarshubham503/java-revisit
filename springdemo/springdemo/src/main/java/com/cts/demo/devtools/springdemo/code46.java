0ef3d658eb4c325d4cab846b96c0b8c4e50001df
Shubham <shubhampawar503@gmail.com>
2022-01-25 22:59:02 +0530

    code to get username from emailid

--- springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code46.java
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code46.java
@@ -1,5 +1,18 @@
 package com.cts.demo.devtools.springdemo;
 
-public class code46 {
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
+public class code46 {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String s=sc.nextLine();
+	System.out.println("user name is:"+user(s));
+}
+public static String user(String s) {
+
+	StringTokenizer st=new StringTokenizer(s,"@");
+	String name=st.nextToken();
+	return name;
+}
 }
