e728499d93b7195a0ab3354f5839693896b02b63
Shubham <shubhampawar503@gmail.com>
2022-03-16 21:40:48 +0530

    61--->find elements in a given integers list in java using stream api

---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code60.java
@@ -0,0 +1,19 @@
+package com.cts.demo.devtools.springdemo;
+
+/*Valid Date
+Given a date string as input, write a program to validate if the given date is i
+n any of the following formats:
+dd.mm.yyyy
+dd/mm/yy
+dd-mm-yyyy*/
+public class code60 {
+
+	public static void main(String[] args) {
+		String s="12.03.2012";
+		getvalues(s);
+	
+	}
+	public static void getvalues(String s) {
+		
+	}
+}
---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/patelcodeEXAMPLE/test.java
@@ -0,0 +1,5 @@
+package com.cts.demo.devtools.springdemo.patelcodeEXAMPLE;
+
+public class test {
+
+}
