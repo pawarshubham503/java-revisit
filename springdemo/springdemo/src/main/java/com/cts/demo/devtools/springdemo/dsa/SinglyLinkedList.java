51a1b949008377e9b7da180b005f1838db11d050
Shubham <shubhampawar503@gmail.com>
2022-09-19 22:02:14 +0530

    insert in the begining of list

--- springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/dsa/SinglyLinkedList.java
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/dsa/SinglyLinkedList.java
@@ -2,22 +2,6 @@ package com.cts.demo.devtools.springdemo.dsa;
 
 public class SinglyLinkedList {
 
-	/*private ListNode head; // instance variable of type list node
-
-	private static class ListNode {// Listnode static class
-
-		// two properties 1-data 2-pointer to next node
-
-		private int data;
-		// generic type
-		private ListNode next;
-
-		public ListNode(int data) {
-			super();
-			this.data = data;
-			this.next = null;
-		}
-		*/
 	private ListNode head;
 
 	private static class ListNode {
@@ -32,40 +16,51 @@ public class SinglyLinkedList {
 
 	public void display() {
 		ListNode current = head;
-		while(current != null) {
+		while (current != null) {
 			System.out.print(current.data + " --> ");
 			current = current.next;
 		}
 		System.out.print("null");
 	}
-		
-		public static void main(String[] args) {
-			SinglyLinkedList sll=new SinglyLinkedList();//initialize the sll
-			
-			//head=null
-			sll.head=new ListNode(10);
-			ListNode second=new ListNode(1);
-			ListNode third =new ListNode(8);
-			ListNode fourth =new ListNode(7);
-			//connect them together to create sll
-			
-			sll.head.next=second;//1
-			second.next=third;//8
-			third.next=fourth;//7
-			
-
-			sll.display();
-			
-			/*System.out.println(sll);
-			ListNode current=sll.head;
-			while(current!=null) {
-				System.out.print(current.data +" -->");
-				current= current.next;
-			}
-			System.out.println("null");*/
+	
+	//to count elements
+	public int length() {
+		if (head==null) {
+			return 0;
 		}
+		ListNode current =head;
+		int count=0;
+		while (current!=null) {
+			count++;
+			current=current.next;
+		}
+		return count;
 		
+	}
+
+	public void insertFirst( int value) {
+		ListNode newNode= new ListNode(value);
+		newNode.next=head;
+		head=newNode;
+	}
+	public static void main(String[] args) {
+		SinglyLinkedList sll = new SinglyLinkedList();// initialize the sll
+
+		// head=null
+		/*
+		 * sll.head = new ListNode(10); ListNode second = new ListNode(1); ListNode
+		 * third = new ListNode(8); ListNode fourth = new ListNode(7); // connect them
+		 * together to create sll
+		 * 
+		 * sll.head.next = second;// 1 second.next = third;// 8 third.next = fourth;// 7
+		 */
+		sll.insertFirst(11);
+		sll.insertFirst(1);
+		sll.insertFirst(2);
+		sll.display();
+		
+		//System.out.println(" length is - " +sll.length());
 
 	}
 
-
+}
