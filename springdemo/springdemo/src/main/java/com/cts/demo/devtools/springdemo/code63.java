d352e1f8d8b835fc1172ca5708074fd52d717aba
Shubham <shubhampawar503@gmail.com>
2022-10-01 19:44:59 +0530

    code to check string is panagram

---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code63.java
@@ -0,0 +1,28 @@
+package com.cts.demo.devtools.springdemo;
+
+public class code63 {
+	
+	//Panagram check code
+	
+	public static void main(String[] args) {
+		String S="the quick brown fox jumps over the lazy dog";
+		System.out.println( isPanagram(S.toLowerCase())); 
+	}
+
+	private static boolean isPanagram(String S) {
+		// TODO Auto-generated method stub
+		if(S.length()<26) {
+			return false;
+			
+		}
+		else {
+			for(char ch='a';ch<='z';ch++) {
+				if(S.indexOf(ch)==0){
+					return false;
+				}
+			}
+		}return true;
+	}
+
+}
+ 
