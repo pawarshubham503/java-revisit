4ce482eb37ab05a099476d6187d72744c881952d
Shubham <shubhampawar503@gmail.com>
2022-10-02 11:42:55 +0530

    code to find occurence of char and longest substring

---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code65.java
@@ -0,0 +1,39 @@
+package com.cts.demo.devtools.springdemo;
+
+import java.util.HashSet;
+
+public class code65 {
+// code to check longest substring
+	
+	public static void main(String[] args) {
+		
+		String str="a,b,c,d,a,b";
+		System.out.println(Longest(str)); 
+	}
+
+private static String Longest(String str) {
+	// TODO Auto-generated method stub
+	HashSet<Character> set= new HashSet<>();
+	String longestOverall="";
+	String longestTillNow="";
+	
+	for (int i=0;i<str.length();i++)
+	{
+		char c=str.charAt(i);
+		if (set.contains(c))
+		{
+			longestTillNow="";
+			set.clear();
+		}
+		set.add(c);
+		longestTillNow+=c;
+		if(longestTillNow.length()>longestOverall.length()) {
+			longestOverall=longestTillNow;
+		}
+	}
+	return longestOverall;
+	
+	
+	
+}
+}
---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code66.java
@@ -0,0 +1,15 @@
+package com.cts.demo.devtools.springdemo;
+
+public class code66 {
+
+	public static void main(String[] args) {
+		String str="www.scaler.com/topics";
+		char ch= 'w';
+		int cnt=0;
+		for (int i=0;i<str.length();i++) {
+			if(str.charAt(i)==ch)
+				cnt ++;
+		}
+		System.out.println("count of  " + ch +" is " +cnt);
+	}
+}
