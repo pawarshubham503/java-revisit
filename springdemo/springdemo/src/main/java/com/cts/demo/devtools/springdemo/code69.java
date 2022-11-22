1f1d387c1bbc68d19cc9c2112c4ac211697b16eb
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2022-11-22 12:14:49 +0000

    code to check if string contains vowels

--- springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code69.java
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code69.java
@@ -1,7 +1,16 @@
 package com.cts.demo.devtools.springdemo;
+//code to check if vowel present in string
 
 public class code69 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println(stringContainsVowels("hello"));//true
+        System.out.println(stringContainsVowels("Tv"));// false
+
+        
     }
+
+    private static boolean stringContainsVowels(String input) {
+        return input.toLowerCase().matches(".*[aeiou].*");
+    }
+    
 }
