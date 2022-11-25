29e65e6b5efaceffe6f7682645fa6750fb9b508c
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2022-11-25 14:35:40 +0000

    string stringbuffer

--- springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code74.java
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code74.java
@@ -2,6 +2,14 @@ package com.cts.demo.devtools.springdemo;
 
 public class code74 {
     public static void main(String[] args) {
+        StringBuffer s1= new StringBuffer("shubham");
+        StringBuffer s2= new StringBuffer("shubham");
+        //String s2=s1;;
+        //s1.concat("pawar");
+        //System.out.println(s1);
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
         
-    }
+
+    } 
 }
