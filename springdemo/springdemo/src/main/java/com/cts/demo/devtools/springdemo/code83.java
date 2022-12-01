8c630e0c25015726f275a0d5a5f24cf87e391e22
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2022-12-01 13:34:30 +0000

    /count number of words in string using hashmap

--- springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code83.java
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code83.java
@@ -1,5 +1,24 @@
 package com.cts.demo.devtools.springdemo;
 
+import java.util.HashMap;
+
 public class code83 {
-    
+    //count number of words in string using hashmap
+    public static void main(String[] args) {
+        String s= "This this is is done by Saket Saket";
+        s=s.toLowerCase();
+        String[] split=s.split(" ");
+        HashMap<String ,Integer> map=new HashMap<String ,Integer>();
+        for(int i=0; i<split.length; i++)
+        {
+            if(map.containsKey(split[i])){
+                int count=map.get(split[i]);
+                map.put(split[i],count+1);
+            }
+            else
+            map.put(split[i], 1);
+        }
+        System.out.println(map);
+    }
+
 }
