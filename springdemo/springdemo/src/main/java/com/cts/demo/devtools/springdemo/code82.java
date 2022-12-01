c1fa02771ea2c2969ea5cbc7098760c935c83cb0
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2022-12-01 12:05:05 +0000

    commit

---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code82.java
@@ -0,0 +1,12 @@
+package com.cts.demo.devtools.springdemo;
+
+public class code82 {
+    //code to reverse string
+    public static void main(String[] args) {
+        String str="shubham";
+        StringBuilder str2=new StringBuilder();
+        str2.append(str);
+        str2=str2.reverse();
+        System.out.println(str2);
+    }
+}
