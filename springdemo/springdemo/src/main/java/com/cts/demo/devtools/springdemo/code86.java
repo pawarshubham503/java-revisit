58d0402052d7a38ce57ac6bd08a18dc5619029b3
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2022-12-02 06:15:18 +0000

    patterns

---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code86.java
@@ -0,0 +1,166 @@
+package com.cts.demo.devtools.springdemo;
+
+public class code86 {
+    public static void main(String[] args) {
+         int n=5;
+
+        // //square pattern
+        
+        // for( int i=1;i<=n;i++){
+        //     for(int j=1;j<=n;j++){
+        //         System.out.print(" * ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        //increasing triangle
+        
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // *
+        // **
+        // ***
+        // ****
+        // *****
+
+
+        //decreasing triangle
+            // for(int i=1;i<=n;i++){
+            //     for(int j=i;j<=n;j++){
+            //         System.out.print("*");
+            //     }
+            //     System.out.println();
+            // }
+
+            // *****
+            // ****
+            // ***
+            // **
+            // *
+
+
+         //right sided triangle
+
+    //      for(int i=1;i<=n;i++){
+    //         for(int j=i;j<=n;j++){
+    //             System.out.print(" "); // print spaces
+    //         }
+    //         for(int j=1;j<=i;j++){
+    //             System.out.print("*"); // print stars
+    //         }
+    //         System.out.println(); //new line
+    //      }
+
+    //       *
+    //      **
+    //     ***
+    //    ****
+    //   *****
+
+
+        //right sided triangle
+
+    //     for(int i=1;i<=n;i++){
+    //         for(int j=1;j<=i;j++){
+    //             System.out.print(" ");
+    //         }
+    //         for(int j=i;j<=n;j++){
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();
+    //     }
+    //    *****
+    //     ****
+    //      ***
+    //       **
+    //        *
+
+
+    //hill pattern
+//      for(int i=1;i<=n;i++){
+//         for(int j=i;j<=n;j++){
+//             System.out.print(" ");
+//         }
+//         for(int j=1;j<i;j++){
+//             System.out.print("*");
+
+//         }
+//         for(int j= 1;j<=i;j++){
+//             System.out.print("*");
+//         }
+//         System.out.println();
+//      }
+//       *
+//      ***
+//     *****
+//    *******
+//   *********
+
+        //revresed hill
+
+    //     for(int i=1;i<=n;i++){
+    //         for(int j=1;j<=i;j++){
+    //             System.out.print(" ");
+    //         }
+    //         for(int j=i;j<n;j++){
+    //             System.out.print("*");
+    //         }
+    //         for(int j=i;j<=n;j++){
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();
+    //     }
+    //    *********
+    //     *******
+    //      *****
+    //       ***
+    //        *
+
+
+    //diamond paTTERN
+
+//     for(int i=1;i<n;i++){
+
+//         for(int j=i;j<=n;j++){
+//             System.out.print(" ");
+//         }
+//         for (int j= 1;j<i;j++){
+//             System.out.print("*");
+//         }
+//         for(int j=1;j<=i;j++){
+//             System.out.print("*");
+//         }
+//         System.out.println();
+//     }
+
+//     for(int i=1;i<=n;i++){
+
+//         for(int j=1;j<=i;j++){
+//             System.out.print(" ");
+//         }
+//         for(int j=i;j<n;j++){
+//             System.out.print("*");
+//         }
+//         for(int j=i;j<=n;j++){
+//             System.out.print("*");
+//         }
+//         System.out.println();
+//     }
+
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+//   *******
+//    *****
+//     ***
+//      *
+    }
+}
