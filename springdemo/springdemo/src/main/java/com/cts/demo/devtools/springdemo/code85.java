5a8f412871829153d768daa87d09fce93d6eab06
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2022-12-02 05:24:22 +0000

    patterns

---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code85.java
@@ -0,0 +1,54 @@
+package com.cts.demo.devtools.springdemo;
+
+public class code85 {
+    public static void main(String[] args) {
+        int i,j,row=6;
+
+        //--- for right triangle
+        //  for( i=0;i<row;i++){
+        //      for(j=0;j<=i;j++){
+        //          System.out.print("*");
+        //     }
+        //      System.out.println();
+        //  }
+
+        // for(i=0;i<row;i++){
+        //     for (j=0;j<=i;j++){
+        //         System.out.print("*");
+        //     }System.out.println();
+        // }
+
+
+        //---for left triangle
+    //     for(i=0;i<row;i++){
+    //         for (j=2*(row-i);j>=0;j--){
+    //             System.out.print(" ");
+    //         }
+    //         for(j=0;j<=i;j++){
+    //             System.out.print("* ");
+    //         }
+    //         System.out.println();
+    //     }
+
+    //--pyramid pattern
+    // for(i=0;i<row;i++){
+    //     for(j=row-i;j>1;j--){
+    //         System.out.print(" ");
+    //     }
+    //     for(j=0;j<=i;j++){
+    //         System.out.print("* ");
+    //     }
+    //     System.out.println();
+    // }
+
+    //inverted decreasing triangle
+    // for(i=0;i<=row;i++){
+    //     for(j=i;j<=row;j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    // }
+
+    
+     }
+}
