dca4a5b9bf13779081c163f593364dadcc3e992d
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2022-12-02 07:41:08 +0000

    foreach

---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code88.java
@@ -0,0 +1,19 @@
+package com.cts.demo.devtools.springdemo;
+
+public class code88 {
+ //for each practice
+ 
+ public static void main(String[] args) {
+    
+    //create array
+
+    int[] num={3,4,5,-5,0,12};
+    int sum=0;
+
+    //iterating through each elements
+    for(int n: num){
+        sum +=n;
+    }
+    System.out.println(sum);
+ }
+}
