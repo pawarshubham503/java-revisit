fe3292fd95b83e2f5acfe08c44e67e62017a4a3f
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2022-12-02 07:28:59 +0000

    foreach code

---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code87.java
@@ -0,0 +1,16 @@
+package com.cts.demo.devtools.springdemo;
+
+public class code87 {
+    public static void main(String[] args) {
+        
+        //code foreach practice
+         
+        //create array
+        int[] num={3,9,5,-5};
+
+        //for each loop
+        for(int n : num){
+            System.out.println(n);
+        }
+    }
+}
