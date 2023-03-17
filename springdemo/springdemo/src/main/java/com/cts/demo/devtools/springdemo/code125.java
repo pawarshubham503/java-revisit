62bad277e2bacfbb651e602a88bedbfabf268b8f
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2023-03-17 14:23:01 +0000

    triangle pattern

--- springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code125.java
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code125.java
@@ -2,7 +2,12 @@ package com.cts.demo.devtools.springdemo;
 
 public class code125 {
     public static void main(String[] args) {
-        
+        int i,j,n=5;
+        for(i=1;i<=n;++i){//rows
+            for(j=1;j<=i;++j){
+                System.out.print("*");
+            }System.out.println();
+        }
     }
     
 }
