6f9975f055fca008aa9f335930e576847d80ad96
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2023-03-17 14:28:51 +0000

    triangle inverted

---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code126.java
@@ -0,0 +1,13 @@
+package com.cts.demo.devtools.springdemo;
+
+public class code126 {
+    public static void main(String[] args) {
+        int i ,j, n=5;
+        for(i=1;i<=n;i++){
+            for(j=i;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
