4906af9b14aea3fea8b144dadf3b05125bb8d705
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2023-03-17 04:18:10 +0000

    triangle pattern

--- springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code118.java
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code118.java
@@ -1,9 +1,15 @@
 package com.cts.demo.devtools.springdemo;
 
-import java.util.Optional;
+
 
 public class code118 {
   public static void main(String[] args) {
-    
+    int rows =5;
+    for(int i=1;i<=rows;++i){
+      for(int j=1;j<=i;++j){
+        System.out.print("*");
+      }
+      System.out.println("");
+    }
 	}
 }
