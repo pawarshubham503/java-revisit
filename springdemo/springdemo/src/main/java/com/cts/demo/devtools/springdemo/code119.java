0e59f01e1846ad3118a9602f1c4aea4fbb212860
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2023-03-17 04:42:06 +0000

    pattern inverted triangle

---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code119.java
@@ -0,0 +1,18 @@
+package com.cts.demo.devtools.springdemo;
+
+public class code119 {
+    public static void main(String[] args) {
+        int row =5;
+        System.out.println("car"); 
+        int i ,j;
+        for(i=row;i>=1;--i ){
+            for(j=1;j<=i;++j){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+       
+
+
+    }
+}
