455d8fcd3b1cfd108e5ac79cedd204b279d8268a
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2023-03-21 16:49:31 +0000

    code to print factors

---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code133.java
@@ -0,0 +1,17 @@
+package com.cts.demo.devtools.springdemo;
+
+import java.util.Scanner;
+
+public class code133 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+
+        for(int i=2; i<n;i++){
+            if(n%i==0){
+                System.out.println(i+ " ");
+            }
+        }
+    sc.close();
+    }
+}
