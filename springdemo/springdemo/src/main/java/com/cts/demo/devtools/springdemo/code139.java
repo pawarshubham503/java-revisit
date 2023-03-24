a6a4216ac2ad96a933cb066e9612afcaa5b85394
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2023-03-24 08:57:14 +0000

    alpha pattern

---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code139.java
@@ -0,0 +1,16 @@
+package com.cts.demo.devtools.springdemo;
+
+public class code139 {
+    public static void main(String[] args) {
+        int ch=65;
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print((char)ch);
+            }
+            System.out.println("");
+            ch++;
+        }
+    }
+    
+}
