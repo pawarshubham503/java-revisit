5f0637301e2ff67aea7b1d1d57739233c26955de
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2023-03-24 09:24:05 +0000

    Cahr pattern

---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code140.java
@@ -0,0 +1,16 @@
+package com.cts.demo.devtools.springdemo;
+
+public class code140 {
+    public static void main(String[] args) {
+        int i,j,n=5;
+        for ( i=0;i<n;i++){
+            int ch=65+i;
+            for (j=0;j<=i;j++){
+              
+                System.out.print((char)ch);
+                ch++;
+            }
+            System.out.println("");
+        }
+    }
+}
