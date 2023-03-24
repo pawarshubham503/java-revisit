a544b0546256ea1b7ebbf8c6570721c860d925eb
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2023-03-24 08:23:31 +0000

    Salary calculator

---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code138.java
@@ -0,0 +1,27 @@
+package com.cts.demo.devtools.springdemo;
+
+import java.util.Scanner;
+
+public class code138 {
+    public static void main(String[] args) {
+      
+        
+        Scanner sc= new Scanner(System.in);
+        int basicS=sc.nextInt(); 
+        int grade=sc.next().charAt(0);  
+        double totalS=basicS+(double)(0.2*basicS)+(double)(0.5*basicS)-(double)(0.11*basicS);
+
+        if (grade==65){
+            totalS=totalS+1700;
+        }
+        else if(grade==66){
+            totalS=totalS+1500;
+        }
+        else{
+            totalS=totalS+1300;
+        }
+
+        System.out.println(Math.round(totalS));
+    }
+    
+}
