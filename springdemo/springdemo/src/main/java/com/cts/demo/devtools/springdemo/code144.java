69329d79fa2735d30cc85340636d7b99f724c48e
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2023-03-24 16:26:49 +0000

    decimal to binary

---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code144.java
@@ -0,0 +1,13 @@
+package com.cts.demo.devtools.springdemo;
+
+import java.util.Scanner;
+
+public class code144 {
+    public static void main(String[] args) {
+        	// Write your code here
+            Scanner scan = new Scanner(System.in);
+            int N=scan.nextInt();
+            System.out.println(Integer.toString(N, 2));        
+    }
+    
+}
