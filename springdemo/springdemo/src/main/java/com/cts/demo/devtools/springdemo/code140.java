7544b42733e2d5c717285b6e54a7b56087d5b69b
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2023-03-24 10:48:17 +0000

    alpha char pattern

--- springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code140.java
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code140.java
@@ -4,7 +4,7 @@ public class code140 {
     public static void main(String[] args) {
         int i,j,n=5;
         for ( i=0;i<n;i++){
-            int ch=65+i;
+            int ch=65+(n-1)-i;
             for (j=0;j<=i;j++){
               
                 System.out.print((char)ch);
--- springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code141.java
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code141.java
@@ -1,5 +1,20 @@
 package com.cts.demo.devtools.springdemo;
 
 public class code141 {
+    public static void main(String[] args) {
+    int n=5;
+    int i,j;
+    for(i=0;i<n;i++){
+        int ch=65+(n-1)-i;
+        for(j=0;j<=i;j++){
+            System.out.print((char)ch);
+            ch++;
+        }
+        System.out.println("");
+    }
+    
+    
+    
+    }
     
 }
