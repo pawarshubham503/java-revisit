3a6ac60ccb3e1a9d81739701f923e930c58b1d8a
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2023-03-26 03:16:08 +0000

    print nth fibonacci series element

---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code146.java
@@ -0,0 +1,21 @@
+package com.cts.demo.devtools.springdemo;
+
+import java.util.Scanner;
+
+public class code146 {
+    public static void main(String[] args) {
+     Scanner sc= new Scanner(System.in);
+     
+     int n=sc.nextInt();
+     int a=1,b=1;
+     int c=0;
+     
+     for (int i=2;i<n;i++){
+        c=a+b;
+        b=c;a=b;
+        
+     }
+     System.out.println(c);
+    }
+    
+}
