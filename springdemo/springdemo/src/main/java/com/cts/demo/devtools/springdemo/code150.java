03d42493a69fe00954752d63882ab378f4a29fc4
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2023-03-27 15:54:08 +0000

    Check fibonacci number

--- springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code150.java
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code150.java
@@ -1,8 +1,33 @@
 package com.cts.demo.devtools.springdemo;
 
+
+
 public class code150 {
+
     public static void main(String[] args) {
-        
+
+System.out.println(checknumber(14));
+
+    }
+
+    
+    public static boolean checknumber(int n){
+        if(n== 0){
+            return true;
+        }
+
+        else{
+            int val1=(5*n*n)-4;
+            int sq1=(int )Math.sqrt(val1);
+            if(sq1*sq1==val1){
+                return true;
+            }
+            else{
+                int val2=(5*n*n)+4;
+                int sq2=(int)Math.sqrt(val2);
+                return(sq2*sq2==val2);
+            }
+        }
     }
     
 }
