d01d9b732231557f6cc5af92190f694a5ef47535
pawarshubham503 <60960905+pawarshubham503@users.noreply.github.com>
2023-03-29 05:02:35 +0000

    even odd

---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code153.java
@@ -0,0 +1,13 @@
+package com.cts.demo.devtools.springdemo;
+//check even or odd 
+public class code153 {
+    public static void main(String[] args) {
+        int num=10;
+        if(num%2==0){
+            System.out.println("even");
+        }
+        else
+        System.out.println("odd");
+    }
+    
+}
