8cdebc3aeea1c2a6fb73a8366a0214b811a0ea5e
Shubham <shubhampawar503@gmail.com>
2024-04-17 09:37:54 +0530

    input from console

---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code161.java
@@ -0,0 +1,15 @@
+package com.cts.demo.devtools.springdemo;
+
+import java.util.Scanner;
+
+public class code161 {
+//code to get input
+
+public static void main(String[] args) {
+    System.out.println("enter valid number");
+    Scanner sc= new Scanner(System.in);
+    int num= sc.nextInt();
+    System.out.println("new number is "+ num);
+    
+}
+}
