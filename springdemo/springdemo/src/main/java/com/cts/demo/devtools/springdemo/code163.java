acc20ab885c513c732793a4d3712da4eb7bdf52b
Shubham <shubhampawar503@gmail.com>
2024-04-25 10:09:53 +0530

    swap number without temp variable

---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code163.java
@@ -0,0 +1,16 @@
+package com.cts.demo.devtools.springdemo;
+
+public class code163 {
+public static void main(String[] args) {
+    int n=5, m=6;
+
+   m=m-n;//1
+   n=m+n; //6
+   m=n-m; //5
+   
+      System.out.println("new m value "+"="+m);          
+   
+      System.out.println("new n value "+"="+n);   
+   
+}
+}
