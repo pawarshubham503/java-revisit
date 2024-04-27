e9e7e918e05d102448dfbbae116149eea9be3e65
Shubham <shubhampawar503@gmail.com>
2024-04-27 09:04:30 +0530

    class+Objects

---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code164.java
@@ -0,0 +1,14 @@
+package com.cts.demo.devtools.springdemo;
+
+public class code164 {
+
+
+    int id=2;
+    String name ="shubham";
+public static void main(String[] args) {
+    
+code164 s1=new code164();
+System.out.println(s1.id);
+System.out.println(s1.name);
+}
+}
