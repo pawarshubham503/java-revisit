2c5962e4c11a5dd8d19216e3a978a34e4e28573c
Shubham <shubhampawar503@gmail.com>
2024-06-18 20:45:26 +0530

    replace char at specific index

--- springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code166.java
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code166.java
@@ -3,6 +3,16 @@ package com.cts.demo.devtools.springdemo;
 public class code166 {
 
     // substring practice
+public static void main(String[] args) {
+    
+    String str="Geeks Gor Geeks";
+    int index=6;
+    char ch='F';
 
+  
+    str = str.substring(0, index) + ch + str.substring(index + 1);
+    System.out.println(str);
+
+}
     
 }
