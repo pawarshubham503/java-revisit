c14281d3a4a81d6c8ee9d1fb4540e02ab8408082
Shubham <shubhampawar503@gmail.com>
2024-06-18 22:30:46 +0530

    comit

--- springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code166.java
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code166.java
@@ -5,13 +5,21 @@ public class code166 {
     // substring practice
 public static void main(String[] args) {
     
+   
     String str="Geeks Gor Geeks";
     int index=6;
     char ch='F';
 
-  
-    str = str.substring(0, index) + ch + str.substring(index + 1);
-    System.out.println(str);
+   // // using String Class
+    // str = str.substring(0, index) + ch + str.substring(index + 1);
+   
+
+
+    // // using string builder
+    StringBuilder string= new StringBuilder(str);
+    string.setCharAt(index, ch);
+
+     System.out.println(str);
 
 }
     
