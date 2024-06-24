408e7a1123f9eae79cef7d75765217ca9328d822
Shubham <shubhampawar503@gmail.com>
2024-06-24 21:12:41 +0530

    reverse string using string builder

--- springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code181.java
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code181.java
@@ -1,5 +1,33 @@
 package com.cts.demo.devtools.springdemo;
 
+
+//swap char in string
+
 public class code181 {
 
+
+
+
+
+    public static void main(String[] args) {
+        String str="GeeksForGeeks";
+        System.out.println(swapPair(str));
+
+    }
+
+    private static String swapPair(String str) {
+        // TODO Auto-generated method stub
+       
+        if(str==null || str.isEmpty())
+        return str;
+        char[] ch=str.toCharArray();
+
+        for(int i=0;i<ch.length -1;i+=2){
+            char temp=ch[i];
+            ch[i]=ch[i+1];
+            ch[i+1]=temp;
+        }
+        return new String(ch);
+        
+    }
 }
---
+++ springdemo/springdemo/src/main/java/com/cts/demo/devtools/springdemo/code182.java
@@ -0,0 +1,12 @@
+package com.cts.demo.devtools.springdemo;
+
+public class code182 {
+
+    public static void main(String[] args) {
+        
+        String str="Hello World";
+        StringBuilder sb=new StringBuilder(str);
+        System.out.println(sb.reverse().toString());
+    }
+
+}
